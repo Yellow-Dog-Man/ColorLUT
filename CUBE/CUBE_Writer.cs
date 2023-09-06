7164f233e95f9d08edb7e0c2a3b1343095560d29
Frooxius <tomas.mariancik@gmail.com>
2023-09-06 02:00:20 +0200

    Add project files.

---
+++ CUBE/CUBE_Writer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColorLUT.CUBE
+{
+    public class CUBE_Writer
+    {
+    }
+}
---
+++ LUT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColorLUT
+{
+    public class LUT
+    {
+        public int Dimensions { get; private set; }
+
+        int[] size;
+        float[] values;
+
+        public LUT(int dimensions, params int[] size)
+        {
+            if (dimensions != size.Length)
+                throw new ArgumentException("Sizes array must match the number of dimensions");
+
+
+        }
+    }
+}
